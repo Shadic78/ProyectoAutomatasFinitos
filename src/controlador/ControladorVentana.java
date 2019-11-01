f5d5fdb2af247619895b8a78a24cf40f465598b3
EusebioAjas <eusebioajas6@gmail.com>
2019-11-01 16:49:19 -0600

    MVC

---
+++ src/controlador/ControladorVentana.java
@@ -0,0 +1,168 @@
+package controlador;
+
+import controlP5.ControlP5;
+import controlP5.Toggle;
+import processing.core.PApplet;
+import static processing.core.PApplet.println;
+import modelo.*;
+import vista.*;
+public class ControladorVentana extends PApplet
+{
+    int estadoDelPrograma = 0;
+    AutomataFinito automata;
+    ControlP5 ventana;
+    public ControlP5 cp5;
+    
+    @Override
+    public void settings()
+    {
+        super.settings(); 
+        fullScreen();
+    }
+    
+    
+    @Override
+    public void setup()
+    {
+        super.setup();
+        VentanaPrincipal ventana1 = new VentanaPrincipal(this);
+        
+        ventana = ventana1.getControlVentanaPrincipal();
+        automata = new AutomataFinito(this);
+        
+        textAlign(CENTER, CENTER);
+        textSize(14);
+        noStroke();
+    }
+    
+    @Override
+    public void draw(){
+    	background(232, 233, 234);
+    	noStroke();
+    	fill(255, 218, 143);
+    	rect(width - 300, 0, 300, height);
+    	
+    	automata.imprimirEstados();
+    	
+    	fill(0);
+    	text("Estado del programa:  " + estadoDelPrograma, width / 3, height - 80);
+    }
+   
+    @Override
+    public void mouseClicked() {
+    	if(mouseX < width - 150) {
+        	switch(estadoDelPrograma) {
+	    		case 1: 
+                            break;
+                        case 2:
+                            EstadoNormal estadoNormal = new EstadoNormal(this, new Punto(mouseX, mouseY, 100), "VerticeNuevo");
+                            automata.agregarEstado(estadoNormal);
+                            break;
+                        default:
+                            break;
+        	}    		
+    	}
+    }
+    
+    public void actualizarEstadoDelPrograma(int estado) {
+    	estadoDelPrograma = estado;
+    	switch(estadoDelPrograma) {
+    		case 1:
+                    ((Toggle) ventana.getController("addEstadoNormal")).setState(false);   
+                    ((Toggle) ventana.getController("addEstadoFinal")).setState(false);
+                    ((Toggle) ventana.getController("addEstadoInicialFinal")).setState(false);
+                    ((Toggle) ventana.getController("moverEstado")).setState(false);
+                    ((Toggle) ventana.getController("borrarEstado")).setState(false);
+                    println("Estado Inicial");
+                    break;
+               /* case 2:
+                    ((Toggle) ventana.getController("addEstadoInicial")).setState(false);   
+                    ((Toggle) ventana.getController("addEstadoFinal")).setState(false);
+                    ((Toggle) ventana.getController("addEstadoInicialFinal")).setState(false);
+                    ((Toggle) ventana.getController("moverEstado")).setState(false);
+                    ((Toggle) ventana.getController("borrarEstado")).setState(false);
+                    println("Estado Normal");        		
+                    break;
+                case 3:
+                    ((Toggle) ventana.getController("addEstadoInicial")).setState(false);   
+                    ((Toggle) ventana.getController("addEstadoNormal")).setState(false);
+                    ((Toggle) ventana.getController("addEstadoInicialFinal")).setState(false);
+                    ((Toggle) ventana.getController("moverEstado")).setState(false);
+                    ((Toggle) ventana.getController("borrarEstado")).setState(false);
+                    println("Estado Final");
+                    break;
+                case 4:
+                    ((Toggle) ventana.getController("addEstadoInicial")).setState(false);   
+                    ((Toggle) ventana.getController("addEstadoNormal")).setState(false);
+                    ((Toggle) ventana.getController("addEstadoFinal")).setState(false);
+                    ((Toggle) ventana.getController("moverEstado")).setState(false);
+                    ((Toggle) ventana.getController("borrarEstado")).setState(false);
+                    println("Estado Inicial-Final");
+                    break;
+                case 5:
+                    ((Toggle) ventana.getController("addEstadoInicial")).setState(false);   
+                    ((Toggle) ventana.getController("addEstadoNormal")).setState(false);
+                    ((Toggle) ventana.getController("addEstadoFinal")).setState(false);
+                    ((Toggle) ventana.getController("addEstadoInicialFinal")).setState(false);
+                    ((Toggle) ventana.getController("borrarEstado")).setState(false);
+                    println("Moviendo Estado");
+                    break;
+                case 6:
+                    ((Toggle) ventana.getController("addEstadoInicial")).setState(false);   
+                    ((Toggle) ventana.getController("addEstadoNormal")).setState(false);
+                    ((Toggle) ventana.getController("addEstadoFinal")).setState(false);
+                    ((Toggle) ventana.getController("addEstadoInicialFinal")).setState(false);
+                    ((Toggle) ventana.getController("moverEstado")).setState(false);
+                    println("Estado Borrado");
+                    break;*/
+                default:
+                    println("Estado default");
+                    break;
+    	}    	
+    }
+    
+    public void addEstadoInicial() {
+    	
+    } 
+    
+    public void addEstadoNormal()
+    {
+        if(((Toggle)ventana.getController("addEstadoNormal")).isMousePressed()) {
+        	if(((Toggle)ventana.getController("addEstadoNormal")).getState() == true) {
+                    actualizarEstadoDelPrograma(2);
+                }
+        	else {
+        		actualizarEstadoDelPrograma(0);
+        	}    		
+    	}
+    }
+    
+    public void addEstadoFinal()
+    {
+        if(((Toggle)ventana.getController("addEstadoFinal")).isMousePressed()) {
+        	if(((Toggle)ventana.getController("addEstadoFinal")).getState() == true) {
+        		actualizarEstadoDelPrograma(3);
+        	}   
+        	else {
+        		actualizarEstadoDelPrograma(0);
+        	}    		
+    	}
+    }
+    
+    public void addEstadoInicialFinal()
+    {
+        
+    }
+    
+    public void borrarEstado()
+    {
+        
+    }
+    
+    public void moverEstado()
+    {
+        
+    }
+    
+    
+}
--- src/vista/VentanaPrincipal.java
+++ src/vista/VentanaPrincipal.java
@@ -1,14 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package vista;
 
-/**
- *
- * @author Usuario
- */
-public class VentanaPrincipal {
+import controlP5.ControlP5;
+import processing.core.PApplet;
+
+public class VentanaPrincipal{
+    public ControlP5 cp5;
+    private PApplet parent;
+    
+    public VentanaPrincipal(PApplet p){
+        this.parent = p;
+        
+        cp5 = new ControlP5(p);
+        
+        //Estados
+        cp5.addToggle("addEstadoInicial")
+    	.setPosition(parent.width - 260, 80)
+    	.setSize(100,40)
+    	.setLabel("Estado inicial")
+    	.getCaptionLabel().align(ControlP5.CENTER, ControlP5.CENTER); 
+    	
+    	cp5.addToggle("addEstadoNormal")
+    	.setPosition(parent.width - 140, 80)
+    	.setSize(100,40)
+    	.setLabel("Estado Normal")
+    	.getCaptionLabel().align(ControlP5.CENTER, ControlP5.CENTER);     
+
+        cp5.addToggle("addEstadoFinal")
+    	.setPosition(parent.width - 260, 140)
+    	.setSize(100,40)
+    	.setLabel("Estado Final")
+    	.getCaptionLabel().align(ControlP5.CENTER, ControlP5.CENTER);
+        
+        cp5.addToggle("addEsatdoInicialFinal")
+    	.setPosition(parent.width - 140, 140)
+    	.setSize(100,40)
+    	.setLabel("Estado Inicial-Final")
+    	.getCaptionLabel().align(ControlP5.CENTER, ControlP5.CENTER);
+        
+        cp5.addToggle("addMoverEstado")
+    	.setPosition(parent.width - 260, 200)
+    	.setSize(100,40)
+    	.setLabel("Mover Estados")
+    	.getCaptionLabel().align(ControlP5.CENTER, ControlP5.CENTER);
+        
+        cp5.addToggle("addBorarEstado")
+    	.setPosition(parent.width - 140, 200)
+    	.setSize(100,40)
+    	.setLabel("Borrar Estado")
+    	.getCaptionLabel().align(ControlP5.CENTER, ControlP5.CENTER);
+    
+        //Conexiones
+        
+        //Ejecutar Automata
+    }
+    
+    public ControlP5 getControlVentanaPrincipal()
+    {
+        return this.cp5;
+    }
+    
+    
     
 }
