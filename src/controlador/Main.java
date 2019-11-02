10e230be9e86707eb660fb5b93c5928beb4a69cf
EusebioAjas <eusebioajas6@gmail.com>
2019-11-01 18:33:25 -0600

    Listo para merge2

--- src/controlador/Main.java
+++ src/controlador/Main.java
@@ -2,7 +2,7 @@ package controlador;
 import processing.core.PApplet;
 
 public class Main {
-
+    //
     public static void main(String[] args)
     {
         PApplet.main("controlador.ControladorVentana");
