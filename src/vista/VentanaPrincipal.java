a0f9fd3ca8b03264a4f1ac97d6950921ee97439a
Shadic78 <carlosjosue7889@gmail.com>
2019-11-02 11:24:57 -0600

    Menu arreglado

--- src/vista/VentanaPrincipal.java
+++ src/vista/VentanaPrincipal.java
@@ -21,37 +21,43 @@ public class VentanaPrincipal{
     }
     
     public void setButtons(ControlP5 b){
-        
-        b.addToggle("Estadoinicial")
-    	.setPosition(parent.width - 250, 80)
+        //Estados
+        b.addToggle("addEstadoInicial")
+    	.setPosition(parent.width - 260, 80)
     	.setSize(100,40)
     	.setLabel("Estado inicial")
     	.getCaptionLabel().align(ControlP5.CENTER, ControlP5.CENTER); 
     	
-    	b.addToggle("EstadoNormal")
-    	.setPosition(parent.width - 130, 80)
+    	b.addToggle("addEstadoNormal")
+    	.setPosition(parent.width - 140, 80)
     	.setSize(100,40)
-    	.setLabel("Estado normal")
+    	.setLabel("Estado Normal")
     	.getCaptionLabel().align(ControlP5.CENTER, ControlP5.CENTER);     
 
-        b.addToggle("EstadoFinal")
-    	.setPosition(parent.width - 250, 130)
+        b.addToggle("addEstadoFinal")
+    	.setPosition(parent.width - 260, 140)
     	.setSize(100,40)
-    	.setLabel("Estado final")
-    	.getCaptionLabel().align(ControlP5.CENTER, ControlP5.CENTER); 
+    	.setLabel("Estado Final")
+    	.getCaptionLabel().align(ControlP5.CENTER, ControlP5.CENTER);
         
-        b.addToggle("BorrarEstado")
-        .setPosition(parent.width - 130, 130)
+        b.addToggle("addEsatdoInicialFinal")
+    	.setPosition(parent.width - 140, 140)
+    	.setSize(100,40)
+    	.setLabel("Estado Inicial-Final")
+    	.getCaptionLabel().align(ControlP5.CENTER, ControlP5.CENTER);
+        
+        b.addToggle("addMoverEstado")
+    	.setPosition(parent.width - 260, 200)
+    	.setSize(100,40)
+    	.setLabel("Mover Estados")
+    	.getCaptionLabel().align(ControlP5.CENTER, ControlP5.CENTER);
+        
+        b.addToggle("addBorarEstado")
+    	.setPosition(parent.width - 140, 200)
     	.setSize(100,40)
     	.setLabel("Borrar Estado")
-    	.getCaptionLabel().align(ControlP5.CENTER, ControlP5.CENTER);
-        
-         b.addToggle("MoverEstado")
-        .setPosition(parent.width - 190, 180)
-    	.setSize(100,40)
-    	.setLabel("Mover Estado")
-    	.getCaptionLabel().align(ControlP5.CENTER, ControlP5.CENTER);
-         
+    	.getCaptionLabel().align(ControlP5.CENTER, ControlP5.CENTER);        
+ 
          b.addToggle("ConexionNormal")
         .setPosition(parent.width - 250, 290)
     	.setSize(100,40)
